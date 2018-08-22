0c35bfaf80ecfe62d06f62bb216db43dbae8b58e
Karol Baraniecki <karol@karcom.pl>
2018-08-22 17:32:49 +0200

    server: keep track of HP

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@
 var net = require('net');
 var msgpack = require('msgpack');
 
+var math = require('./math.js');
+
 // fix exiting in Docker
 process.on('SIGINT', function() {
     process.exit();
 });
+
+const PLAYER_RADIUS = 15;
 
 const TIMEOUT_KICK = 60000; // 60 s
 
@@ -14,6 +18,7 @@ const MSGS_SEND = {
 	NEW_PLAYER: 0,
 	PLAYER_MOVE: 1,
 	SHOT: 2,
+	SET_HP: 3
 };
 
 const MSGS_RECEIVE = {
@@ -29,6 +34,7 @@ function newClientId() {
 
 var allClients = [];
 
+
 // after TIMEOUT_KICK ms of no data a socket is considered disconnected.
 function resetTimeout(socket) {
 	socket.disconnectTimeout.refresh();
@@ -43,13 +49,15 @@ function messageToSocket(socket, message) {
 }
 
 // send all existing players' nicknames to a newly connected player
-function sendAllPlayersNicknames(socket) {
+function sendGameStateToNewPlayer(socket) {
 	var message = [];
 	for (var clientIdx in allClients) {
 		var client = allClients[clientIdx];
 		if(client.nickname != null) {
 			message.push(MSGS_SEND.NEW_PLAYER);
 			message.push([client.clientId, client.nickname]);
+			message.push(MSGS_SEND.SET_HP);
+			message.push([client.clientId, client.hp]);
 		}
 	}
 	messageToSocket(socket, message);
@@ -63,6 +71,49 @@ function sendToAllExcept(clientId, message) {
 	})
 }
 
+function weaponDamage(weaponId) {
+	const weaponDamages = [
+		1,
+		1,
+		2,
+		3,
+		10,
+		1,
+		2,
+		5,
+		1,
+		3,
+		1,
+		1
+	];
+
+	return weaponDamages[weaponId] || 1;
+}
+
+function decreaseHp({ hit, shooter }) {
+	var damage = weaponDamage(shooter.weapon);
+	console.log(`Decreased hp of ${hit.clientId} from ${hit.hp} to ${hit.hp - damage}`);
+	hit.hp -= damage;
+	sendToAllExcept(hit.clientId, [MSGS_SEND.SET_HP, [hit.clientId, hit.hp, shooter.clientId]])
+	messageToSocket(hit.socket, [MSGS_SEND.SET_HP, [-1, hit.hp, shooter.clientId]]);
+}
+
+function handleShot(shootingClient, bulletEnd) {
+	allClients.forEach(otherPlayer => {
+		if(otherPlayer.clientId == shootingClient.clientId)
+			return; // skip the shooter
+
+		console.log(shootingClient.x, shootingClient.y, bulletEnd.x, bulletEnd.y, otherPlayer.x, otherPlayer.y, PLAYER_RADIUS);
+		if(math.line_intersection_with_circle(
+			shootingClient.x, shootingClient.y,
+			bulletEnd.x, bulletEnd.y,
+			otherPlayer.x, otherPlayer.y, PLAYER_RADIUS)) {
+
+			decreaseHp({ hit: otherPlayer, shooter: shootingClient });
+		}
+	});
+}
+
 // All message types possible to receive from the client
 var handleMessage = []
 handleMessage[MSGS_RECEIVE.SET_NICKNAME] = function(clientInfo, arg) {
@@ -74,7 +125,7 @@ handleMessage[MSGS_RECEIVE.SET_NICKNAME] = function(clientInfo, arg) {
 }
 handleMessage[MSGS_RECEIVE.PLAYER_MOVE] = function(clientInfo, arg) {
 	// arg is [x:float, y:float, flags:short, weapon:int]
-	if(!Array.isArray(arg) || arg.length != 3) {
+	if(!Array.isArray(arg) || arg.length != 4) {
 		console.warn(`Malformed PLAYER_MOVE from client ${clientInfo.clientId}`);
 		return;
 	}
@@ -97,7 +148,12 @@ handleMessage[MSGS_RECEIVE.SHOT_WEAPON] = function(clientInfo, arg) {
 		console.warn(`SHOT from ${clientInfo.clientId}: argument is not a number array: ${arg}`);
 		return;
 	}
+
 	sendToAllExcept(clientInfo.clientId, [MSGS_SEND.SHOT, [ clientInfo.clientId ].concat(arg)]);
+
+	for(var i = 0, len = arg.length; i < len; i += 2) {
+		handleShot(clientInfo, new math.Point(arg[i], arg[i + 1]));
+	}
 }
 
 
@@ -111,7 +167,7 @@ var server = net.createServer(function(socket) {
 		socket.end();
 	}, TIMEOUT_KICK);
 
-	sendAllPlayersNicknames(socket);
+	sendGameStateToNewPlayer(socket);
 	//messageToSocket(socket, [MSGS_SEND.PLAYER_MOVE, [123, 1.5, 1.5, 1.5, 1.5]])
 
 	var clientInfo = {
@@ -120,7 +176,9 @@ var server = net.createServer(function(socket) {
 		x: 0,
 		y: 0,
 		flags: 0,
-		socket
+		weapon: 0,
+		hp: 5,
+		socket,
 	}
 
 	allClients[clientInfo.clientId] = clientInfo;
